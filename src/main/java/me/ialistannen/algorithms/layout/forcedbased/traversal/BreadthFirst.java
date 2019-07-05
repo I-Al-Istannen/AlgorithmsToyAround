a1e054b9237d986d20b100d0652b5fbfa2d72047
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-07-05 07:39:21 +0200

    Add a breadth first traversal

---
+++ src/main/java/me/ialistannen/algorithms/layout/forcedbased/traversal/BreadthFirst.java
@@ -0,0 +1,51 @@
+package me.ialistannen.algorithms.layout.forcedbased.traversal;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import me.ialistannen.algorithms.layout.forcedbased.tree.Node;
+
+/**
+ * A Breadth first traversal.
+ */
+public class BreadthFirst implements Traversal {
+
+  @Override
+  public <T> List<NodeChangeAction<T>> run(List<Node<T>> nodes) {
+    List<NodeChangeAction<T>> result = new ArrayList<>();
+
+    Queue<Node<T>> thisLayer = new ArrayDeque<>();
+    Queue<Node<T>> nextLayer = new ArrayDeque<>();
+    Set<Node<T>> visited = new HashSet<>();
+
+    thisLayer.add(nodes.get(0));
+
+    int counter = 0;
+    for (int level = 1; !thisLayer.isEmpty(); level++) {
+
+      while (!thisLayer.isEmpty()) {
+        Node<T> node = thisLayer.poll();
+        for (Node<T> neighbour : node.getNeighbours()) {
+          if (!visited.add(neighbour)) {
+            continue;
+          }
+          result.add(
+              NodeChangeAction.builder(neighbour)
+                  .withLeftText("" + level)
+                  .withRightText("" + ++counter)
+                  .build()
+          );
+          nextLayer.add(neighbour);
+        }
+      }
+
+      thisLayer = new ArrayDeque<>(nextLayer);
+      nextLayer.clear();
+    }
+
+    return result;
+  }
+}
